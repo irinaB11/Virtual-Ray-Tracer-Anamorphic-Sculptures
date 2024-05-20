431e5efd8367c303e10778e9d8d5869579ac7c9d
irinaB11 <bodolairina@gmail.com>
2024-05-20 22:01:04 +0200

    attempt at constructing anamorphic deformation function

---
+++ Unity/Assets/AnamorphicDeformation.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class AnamorphicDeformation : MonoBehaviour
+{
+    public GameObject highVertexCube;
+    public GameObject cylinderMirror;
+    public GameObject eye; //right now it is represented by a sphere in the scene
+
+}
